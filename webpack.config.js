63490ca07babb6c64185d6c39b65c459e4140fb1
Aleksey Kosolapov <klealekle@gmail.com>
2023-12-10 18:15:29 +0300

    mediakt

--- webpack.config.js
+++ webpack.config.js
@@ -197,149 +197,149 @@ const config = {
                 //useShortDoctype: true
             }
         }),
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: `${PATHS.src}/404.html`,
-            filename: './404.html'
-        }),
+        // new HtmlWebpackPlugin({
+        //     hash: false,
+        //     template: `${PATHS.src}/404.html`,
+        //     filename: './404.html'
+        // }),
 
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: `${PATHS.src}/tours.html`,
-            filename: './tours.html'
-        }),
+        // new HtmlWebpackPlugin({
+        //     hash: false,
+        //     template: `${PATHS.src}/tours.html`,
+        //     filename: './tours.html'
+        // }),
 
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: `${PATHS.src}/matches.html`,
-            filename: './matches.html'
-        }),
+        // new HtmlWebpackPlugin({
+        //     hash: false,
+        //     template: `${PATHS.src}/matches.html`,
+        //     filename: './matches.html'
+        // }),
 
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: `${PATHS.src}/match_live.html`,
-            filename: './match_live.html'
-        }),
+        // new HtmlWebpackPlugin({
+        //     hash: false,
+        //     template: `${PATHS.src}/match_live.html`,
+        //     filename: './match_live.html'
+        // }),
 
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: `${PATHS.src}/match_past.html`,
-            filename: './match_past.html'
-        }),
+        // new HtmlWebpackPlugin({
+        //     hash: false,
+        //     template: `${PATHS.src}/match_past.html`,
+        //     filename: './match_past.html'
+        // }),
 
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: `${PATHS.src}/match_pubg.html`,
-            filename: './match_pubg.html'
-        }),
+        // new HtmlWebpackPlugin({
+        //     hash: false,
+        //     template: `${PATHS.src}/match_pubg.html`,
+        //     filename: './match_pubg.html'
+        // }),
 
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: `${PATHS.src}/match_pubg_past.html`,
-            filename: './match_pubg_past.html'
-        }),
+        // new HtmlWebpackPlugin({
+        //     hash: false,
+        //     template: `${PATHS.src}/match_pubg_past.html`,
+        //     filename: './match_pubg_past.html'
+        // }),
 
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: `${PATHS.src}/page_text.html`,
-            filename: './page_text.html'
-        }),
+        // new HtmlWebpackPlugin({
+        //     hash: false,
+        //     template: `${PATHS.src}/page_text.html`,
+        //     filename: './page_text.html'
+        // }),
 
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: `${PATHS.src}/user_account.html`,
-            filename: './user_account.html'
-        }),
+        // new HtmlWebpackPlugin({
+        //     hash: false,
+        //     template: `${PATHS.src}/user_account.html`,
+        //     filename: './user_account.html'
+        // }),
 
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: `${PATHS.src}/user_log.html`,
-            filename: './user_log.html'
-        }),
+        // new HtmlWebpackPlugin({
+        //     hash: false,
+        //     template: `${PATHS.src}/user_log.html`,
+        //     filename: './user_log.html'
+        // }),
 
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: `${PATHS.src}/user_reg.html`,
-            filename: './user_reg.html'
-        }),
+        // new HtmlWebpackPlugin({
+        //     hash: false,
+        //     template: `${PATHS.src}/user_reg.html`,
+        //     filename: './user_reg.html'
+        // }),
 
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: `${PATHS.src}/news.html`,
-            filename: './news.html'
-        }),
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: `${PATHS.src}/news_v2.html`,
-            filename: './news_v2.html'
-        }),
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: `${PATHS.src}/news_list.html`,
-            filename: './news_list.html'
-        }),
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: `${PATHS.src}/news_list_word.html`,
-            filename: './news_list_word.html'
-        }),
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: `${PATHS.src}/elements.html`,
-            filename: './elements.html'
-        }),
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: `${PATHS.src}/players_list.html`,
-            filename: './players_list.html'
-        }),
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: `${PATHS.src}/players_list_pubg.html`,
-            filename: './players_list_pubg.html'
-        }),
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: `${PATHS.src}/teams_list.html`,
-            filename: './teams_list.html'
-        }),
+        // new HtmlWebpackPlugin({
+        //     hash: false,
+        //     template: `${PATHS.src}/news.html`,
+        //     filename: './news.html'
+        // }),
+        // new HtmlWebpackPlugin({
+        //     hash: false,
+        //     template: `${PATHS.src}/news_v2.html`,
+        //     filename: './news_v2.html'
+        // }),
+        // new HtmlWebpackPlugin({
+        //     hash: false,
+        //     template: `${PATHS.src}/news_list.html`,
+        //     filename: './news_list.html'
+        // }),
+        // new HtmlWebpackPlugin({
+        //     hash: false,
+        //     template: `${PATHS.src}/news_list_word.html`,
+        //     filename: './news_list_word.html'
+        // }),
+        // new HtmlWebpackPlugin({
+        //     hash: false,
+        //     template: `${PATHS.src}/elements.html`,
+        //     filename: './elements.html'
+        // }),
+        // new HtmlWebpackPlugin({
+        //     hash: false,
+        //     template: `${PATHS.src}/players_list.html`,
+        //     filename: './players_list.html'
+        // }),
+        // new HtmlWebpackPlugin({
+        //     hash: false,
+        //     template: `${PATHS.src}/players_list_pubg.html`,
+        //     filename: './players_list_pubg.html'
+        // }),
+        // new HtmlWebpackPlugin({
+        //     hash: false,
+        //     template: `${PATHS.src}/teams_list.html`,
+        //     filename: './teams_list.html'
+        // }),
 
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: `${PATHS.src}/teams_list2.html`,
-            filename: './teams_list2.html'
-        }),
+        // new HtmlWebpackPlugin({
+        //     hash: false,
+        //     template: `${PATHS.src}/teams_list2.html`,
+        //     filename: './teams_list2.html'
+        // }),
 
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: `${PATHS.src}/tour.html`,
-            filename: './tour.html'
-        }),
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: `${PATHS.src}/tour_stadia.html`,
-            filename: './tour_stadia.html'
-        }),
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: `${PATHS.src}/tour_group.html`,
-            filename: './tour_group.html'
-        }),
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: `${PATHS.src}/match_HS.html`,
-            filename: './match_HS.html'
-        }),
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: `${PATHS.src}/player_pubg.html`,
-            filename: './player_pubg.html'
-        }),
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: `${PATHS.src}/player_hs.html`,
-            filename: './player_hs.html'
-        }),
+        // new HtmlWebpackPlugin({
+        //     hash: false,
+        //     template: `${PATHS.src}/tour.html`,
+        //     filename: './tour.html'
+        // }),
+        // new HtmlWebpackPlugin({
+        //     hash: false,
+        //     template: `${PATHS.src}/tour_stadia.html`,
+        //     filename: './tour_stadia.html'
+        // }),
+        // new HtmlWebpackPlugin({
+        //     hash: false,
+        //     template: `${PATHS.src}/tour_group.html`,
+        //     filename: './tour_group.html'
+        // }),
+        // new HtmlWebpackPlugin({
+        //     hash: false,
+        //     template: `${PATHS.src}/match_HS.html`,
+        //     filename: './match_HS.html'
+        // }),
+        // new HtmlWebpackPlugin({
+        //     hash: false,
+        //     template: `${PATHS.src}/player_pubg.html`,
+        //     filename: './player_pubg.html'
+        // }),
+        // new HtmlWebpackPlugin({
+        //     hash: false,
+        //     template: `${PATHS.src}/player_hs.html`,
+        //     filename: './player_hs.html'
+        // }),
         // new HtmlWebpackPlugin({
         //     hash: false,
         //     template: `${PATHS.src}/tour_group_stadia.html`,
@@ -382,51 +382,57 @@ const config = {
         //     template: `${PATHS.src}/search.html`,
         //     filename: './search.html'
         // }),
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: `${PATHS.src}/match_cs_past.html`,
-            filename: './match_cs_past.html'
-        }),
+        // new HtmlWebpackPlugin({
+        //     hash: false,
+        //     template: `${PATHS.src}/match_cs_past.html`,
+        //     filename: './match_cs_past.html'
+        // }),
         // new HtmlWebpackPlugin({
         //     hash: false,
         //     template: `${PATHS.src}/job.html`,
         //     filename: './job.html'
         // }),
+        // new HtmlWebpackPlugin({
+        //     hash: false,
+        //     template: `${PATHS.src}/transfers.html`,
+        //     filename: './transfers.html'
+        // }),
+        // new HtmlWebpackPlugin({
+        //     hash: false,
+        //     template: `${PATHS.src}/team2.html`,
+        //     filename: './team2.html'
+        // }),
+        // new HtmlWebpackPlugin({
+        //     hash: false,
+        //     template: `${PATHS.src}/mail.html`,
+        //     filename: './mail.html'
+        // }),
+        // new HtmlWebpackPlugin({
+        //     hash: false,
+        //     template: `${PATHS.src}/pwd.html`,
+        //     filename: './pwd.html'
+        // }),
+        // new HtmlWebpackPlugin({
+        //     hash: false,
+        //     template: `${PATHS.src}/pwd_count.html`,
+        //     filename: './pwd_count.html'
+        // }),
+        // new HtmlWebpackPlugin({
+        //     hash: false,
+        //     template: `${PATHS.src}/reg.html`,
+        //     filename: './reg.html'
+        // }),
+        // new HtmlWebpackPlugin({
+        //     hash: false,
+        //     template: `${PATHS.src}/externallink.html`,
+        //     filename: './externallink.html'
+        // }),
         new HtmlWebpackPlugin({
             hash: false,
-            template: `${PATHS.src}/transfers.html`,
-            filename: './transfers.html'
-        }),
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: `${PATHS.src}/team2.html`,
-            filename: './team2.html'
-        }),
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: `${PATHS.src}/mail.html`,
-            filename: './mail.html'
-        }),
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: `${PATHS.src}/pwd.html`,
-            filename: './pwd.html'
-        }),
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: `${PATHS.src}/pwd_count.html`,
-            filename: './pwd_count.html'
-        }),
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: `${PATHS.src}/reg.html`,
-            filename: './reg.html'
-        }),
-        new HtmlWebpackPlugin({
-            hash: false,
-            template: `${PATHS.src}/externallink.html`,
-            filename: './externallink.html'
+            template: `${PATHS.src}/media_kit.html`,
+            filename: './media_kit.html'
         })
+
 
     ],
 };
